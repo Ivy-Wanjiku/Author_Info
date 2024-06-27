48e39998c9fdf63faadc5736f3fa0baf201627d8
Ivy-Wanjiku <wanjiruwanjikuivy@gmail.com>
2024-06-27 16:30:51 +0300

    add files

---
+++ app/src/main/java/com/akirachix/myreaderblog/Authors.kt
@@ -0,0 +1,12 @@
+package com.akirachix.myreaderblog
+
+data class Authors(
+    var authors:String,
+    var name:String,
+    var title:String,
+    var publisherDate:String,
+    var articlePreview:String,
+    var articlePhoto:String,
+    var link:String
+
+)
