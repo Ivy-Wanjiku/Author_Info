a2a273372a8c7d417346eb843a25a1e3e3b5686d
Ivy-Wanjiku <wanjiruwanjikuivy@gmail.com>
2024-06-27 21:34:33 +0300

    update files

--- app/src/main/java/com/akirachix/myreaderblog/MainActivity.kt
+++ app/src/main/java/com/akirachix/myreaderblog/MainActivity.kt
@@ -15,6 +15,22 @@ class MainActivity : AppCompatActivity() {
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.rvInfo.layoutManager=LinearLayoutManager(this)
-
+        displayAuthor()
+    }
+    fun displayAuthor(){
+        val author1=Authors("","Ngugi","I will marry when I want","2046/575"," "," "," ")
+        val author2=Authors("","Ngugi","I will marry when I want","2046/575"," "," "," ")
+        val author3=Authors("","Ngugi","I will marry when I want","2046/575"," "," "," ")
+        val author4=Authors("","Ngugi","I will marry when I want","2046/575"," "," "," ")
+        val author5=Authors("","Ngugi","I will marry when I want","2046/575"," "," "," ")
+        val author6=Authors("","Ngugi","I will marry when I want","2046/575"," "," "," ")
+        val author7=Authors("","Ngugi","I will marry when I want","2046/575"," "," "," ")
+        val author8=Authors("","Ngugi","I will marry when I want","2046/575"," "," "," ")
+        val author9=Authors("","Ngugi","I will marry when I want","2046/575"," "," "," ")
+        val author10=Authors("","Ngugi","I will marry when I want","2046/575"," "," "," ")
+        val author11=Authors("","Ngugi","I will marry when I want","2046/575"," "," "," ")
+val myauthors= listOf(author1,author2,author3,author4,author5,author6,author7,author8,author9,author10,author11)
+        val authorsAdapter=AuthorsAdapter(myauthors)
+        binding.rvInfo.adapter=authorsAdapter
     }
 }
