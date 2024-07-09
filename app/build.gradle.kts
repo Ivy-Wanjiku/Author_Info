783db84257e7c26ebb17c4fc5e25ff4ff72f0f0e
Ivy-Wanjiku <wanjiruwanjikuivy@gmail.com>
2024-07-09 19:01:20 +0300

    updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation ("com.squareup.picasso:picasso:2.8")
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
